4a2e097db3fb28efb81ff72e76b2d591c56a4e12
Elijah Basile <berlinboy9@yahoo.com>
2018-05-15 11:04:19 -0400

    use a number for age

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const form = document.querySelector('form#userForm')
 const handleSubmit = function(ev) {
     ev.preventDefault();
+    const form = ev.target
     const userName = form.userName.value;
     const users = document.querySelector('#users')
+    const age = form.age.value;
     heading.textContent = document.querySelector('input').value;
-    users.innerHTML += '<p>' + userName + '</p>'
-    form.userName.value = ''
+    users.innerHTML += '<p>' + userName + ', '+ age +'</p>'
+    form.reset()
+    form.userName.focus()
 
 }
 form.addEventListener('submit', handleSubmit)
