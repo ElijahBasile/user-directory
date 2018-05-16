0077625a1f0babd056eccbe642e993c18ad04e29
Elijah Basile <berlinboy9@yahoo.com>
2018-05-16 11:19:52 -0400

    arrow operator

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('#userForm')
-const renderListItem = function(label, item) {
+const renderListItem = (label, item) => {
     const listItem = document.createElement('li')
     listItem.textContent = `${label}: `
     try{
@@ -9,7 +9,7 @@ const renderListItem = function(label, item) {
     }
     return listItem;
 }
-const renderList = function(data) {
+const renderList = (data) => {
     const list = document.createElement('ul')
     Object.keys(data).map(function(label) {
         const item = renderListItem(label, data[label])
@@ -17,14 +17,14 @@ const renderList = function(data) {
     })
     return list
 }
-const renderColor = function(favcolor) {
+const renderColor = (favcolor) => {
     const colorDiv = document.createElement('div')
     colorDiv.style.width = '6rem'
     colorDiv.style.height = '3rem'
     colorDiv.style.backgroundColor = favcolor
     return colorDiv
 }
-const handleSubmit = function(ev) {
+const handleSubmit = (ev) => {
     ev.preventDefault();
     const form = ev.target
     const user = {
